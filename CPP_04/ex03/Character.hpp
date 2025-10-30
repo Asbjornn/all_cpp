e9889eb7c9c170740e9031cdd147af2d397ad182
Gabin Cauchy <gcauchy@shi-c1-p2.s19.be>
2025-10-30 16:20:43 +0100

    :tada:

--- CPP_04/ex03/Character.hpp
+++ CPP_04/ex03/Character.hpp
@@ -6,19 +6,22 @@
 /*   By: gcauchy <gcauchy@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:40:04 by gcauchy           #+#    #+#             */
-/*   Updated: 2025/10/30 10:41:35 by gcauchy          ###   ########.fr       */
+/*   Updated: 2025/10/30 16:03:42 by gcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHARACTER_HPP
-#define CHARACTER_HPP
+# define CHARACTER_HPP
 
 #include <iostream>
-#include "ICharacter.hpp"
 
-class ICharacter {
+class Character {
     public:
-        
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
 };
 
 #endif
--- CPP_04/ex03/MateriaSource.hpp
+++ CPP_04/ex03/MateriaSource.hpp
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcauchy <gcauchy@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/28 17:00:07 by gcauchy           #+#    #+#             */
-/*   Updated: 2025/10/30 14:36:30 by gcauchy          ###   ########.fr       */
+/*   Created: 2025/10/30 10:56:17 by gcauchy           #+#    #+#             */
+/*   Updated: 2025/10/30 10:56:33 by gcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ICHARACTER_HPP
-#define ICHARACTER_HPP
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
 
 #include <iostream>
-#include "AMateria.hpp"
 
-class ICharacter {
+class MateriaSource {
     public:
-        virtual ~ICharacter() {}
-        virtual std::string const & getName() const = 0;
-        virtual void equip(AMateria* m) = 0;
-        virtual void unequip(int idx) = 0;
-        virtual void use(int idx, ICharacter& target) = 0;
+
 };
 
 #endif
