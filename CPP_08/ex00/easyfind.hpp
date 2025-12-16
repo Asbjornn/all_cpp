faa0d209cec86491be460f38e3628eabf3fb47a1
Gabin Cauchy <gcauchy@shi-r1-p2.42belgium.be>
2025-12-16 16:20:29 +0100

    :tada:

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: gcauchy <gcauchy@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:47:54 by gcauchy           #+#    #+#             */
-/*   Updated: 2025/12/16 11:21:38 by gcauchy          ###   ########.fr       */
+/*   Updated: 2025/12/16 14:29:04 by gcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@
 
 template <typename T>
 void easyfind(const T& container, int n) {
+	int a = 0;;
     for (typename T::const_iterator i = container.begin(); i != container.end() ; i++)
     {
         if (*i == n)
         {
-            std::cout << "FOUND " << n << " at index: " << *i << std::endl;
+            std::cout << "FOUND " << n << " at index: " << a << std::endl;
             return ;
         }
+		a++;
     }
     std::cout << "pas la ?" << std::endl;
 }
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,22 +6,23 @@
 /*   By: gcauchy <gcauchy@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:47:24 by gcauchy           #+#    #+#             */
-/*   Updated: 2025/12/16 11:21:59 by gcauchy          ###   ########.fr       */
+/*   Updated: 2025/12/16 14:29:39 by gcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
+#include <deque>
 #include "easyfind.hpp"
 
 int main(void) {
-    std::vector<int> tab;
-    tab.push_back(1);
-    tab.push_back(2);
-    tab.push_back(3);
-    tab.push_back(4);
-    tab.push_back(5);
+    std::vector<int> tab = {1, 2, 3, 4, 5};
 
     easyfind(tab, 3);
     easyfind(tab, 40);
+
+	std::deque<int> deque = {10, 9, 8, 7, 6, 4, 2};
+
+	easyfind(deque, 6);
+    easyfind(deque, 3);
     return 0;
 }
